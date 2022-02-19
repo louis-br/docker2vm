3ce10a4db1bc082316c7c1ba3c91410b8af2459c
louis-br <46930119+louis-br@users.noreply.github.com>
2022-02-19 14:41:23 -0300

    Move bootloaders and distros into folders

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-export COMPOSE_FILE=docker-compose.boot.yml:docker-compose.setup.yml:docker-compose.disk.yml
+#BOOTLOADER=${BOOTLOADER:-grub}
+#DIR=${DIR:-/output}
+#TOTAL_SIZE=${TOTAL_SIZE:-2GB}
+#EFI_SIZE=${EFI_SIZE:-261MiB}
+#LABEL=${LABEL:-rootfs}
+
+export COMPOSE_FILE=docker-compose.bootloader.yml:docker-compose.distro.yml:docker-compose.disk.yml
+#export COMPOSE_PROFILES=grub,ubuntu
+#docker-compose config
 docker-compose build
 
-export COMPOSE_FILE=docker-compose.boot.yml:docker-compose.setup.yml
+export COMPOSE_FILE=docker-compose.bootloader.yml:docker-compose.distro.yml
 docker-compose up --remove-orphans
-docker export --output ./output/rootfs.tar $(docker-compose ps ubuntu -q)
+docker export --output ./output/rootfs.tar $(docker-compose ps -q)
 docker-compose down
 
 export COMPOSE_FILE=docker-compose.disk.yml
