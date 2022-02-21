d5c3bbb6e4b0b9e18038e636f52c09e0b9990c41
louis-br <46930119+louis-br@users.noreply.github.com>
2022-02-21 08:47:25 -0300

    Split into multiple compose files

--- build.sh
+++ build.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-#BOOTLOADER=${BOOTLOADER:-grub}
-#DIR=${DIR:-/output}
-#TOTAL_SIZE=${TOTAL_SIZE:-2GB}
-#EFI_SIZE=${EFI_SIZE:-261MiB}
-#LABEL=${LABEL:-rootfs}
-
 . ./env.sh
-export COMPOSE_FILE=docker-compose.bootloader.yml:docker-compose.distro.yml:docker-compose.disk.yml
+
+BOOTLOADER_PATH="bootloaders/${BOOTLOADER}/docker-compose.yml"
+DISTRO_PATH="distros/${DISTRO}/docker-compose.yml"
+export COMPOSE_FILE=docker-compose.base.yml:docker-compose.disk.yml:${BOOTLOADER_PATH}:${DISTRO_PATH}
 docker-compose build
 
-export COMPOSE_FILE=docker-compose.bootloader.yml
+export COMPOSE_FILE=docker-compose.base.yml:${BOOTLOADER_PATH}
 docker-compose up --remove-orphans
 docker-compose down
-export COMPOSE_FILE=docker-compose.distro.yml
+export COMPOSE_FILE=docker-compose.base.yml:${DISTRO_PATH}
 docker-compose up --remove-orphans
 docker export --output ./output/rootfs.tar $(docker-compose ps -q)
 docker-compose down
