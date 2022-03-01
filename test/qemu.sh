e988c9ebd6f1d53b5a9f04c311b6b6a992448b87
louis-br <46930119+louis-br@users.noreply.github.com>
2022-03-01 15:02:21 -0300

    Add basic networking support

--- test/qemu.sh
+++ test/qemu.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo qemu-system-x86_64 -m 2048M -bios /usr/share/ovmf/OVMF.fd -device virtio-blk-pci,drive=disk0,bootindex=0 -drive id=disk0,if=none,format=raw,file=./output/disk.img
+sudo qemu-system-x86_64 \
+    -m 2048M \
+    -bios /usr/share/ovmf/OVMF.fd \
+    -device virtio-blk-pci,drive=disk0,bootindex=0 \
+    -drive id=disk0,if=none,format=raw,file=./output/disk.img \
+    -device e1000,netdev=net0 \
+    -netdev user,id=net0
