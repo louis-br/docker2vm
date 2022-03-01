2ad8cc7a7d9f71992275afe7d4036f319640dc73
louis-br <46930119+louis-br@users.noreply.github.com>
2022-03-01 15:01:21 -0300

    Stop on failures

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@
 BOOTLOADER_PATH="bootloaders/${BOOTLOADER}/docker-compose.yml"
 DISTRO_PATH="distros/${DISTRO}/docker-compose.yml"
 export COMPOSE_FILE=docker-compose.base.yml:docker-compose.disk.yml:${BOOTLOADER_PATH}:${DISTRO_PATH}
-docker-compose build
+docker-compose build || exit 1
 
 export COMPOSE_FILE=docker-compose.base.yml:${BOOTLOADER_PATH}
-docker-compose up --remove-orphans
-docker-compose down
+docker-compose up --remove-orphans || exit 1
+#docker-compose down
 export COMPOSE_FILE=docker-compose.base.yml:${DISTRO_PATH}
-docker-compose up --remove-orphans
-docker export --output ./output/rootfs.tar $(docker-compose ps -q)
-docker-compose down
+docker-compose up --remove-orphans || exit 1
+docker export --output ./output/rootfs.tar $(docker-compose ps -q) || exit 1
+#docker-compose down
 
 export COMPOSE_FILE=docker-compose.disk.yml
-docker-compose up --remove-orphans
-docker-compose down
+docker-compose up --remove-orphans || exit 1
+#docker-compose down
